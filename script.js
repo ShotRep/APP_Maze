affa4ae20a5e4d164dd7253d4ce1fb7f4eda8ef6
ShotRep <78008209+ShotRep@users.noreply.github.com>
2021-08-02 09:10:21 -0500

    Finished maze application with WASD key functionality.

--- script.js
+++ script.js
@@ -7,18 +7,19 @@ console.log(Matter)
 //Create
 const engine = Engine.create() //this creates a world object
 engine.world.gravity.y = 0
+engine.world.gravity.x = 0
 const {world} = engine
 console.log(world)
 
-const width = 600
-const height = 600
+const width = window.innerWidth - 9
+const height = window.innerHeight - 9
 
 const render = Render.create({
   element: document.body, //options object showing where to render
   engine: engine, //point ot our created engine
   options: {
     // size on canvas
-    wireframes: true,
+    wireframes: false,
     width, //we will add to this later-remove static sizes
     height,
   },
@@ -72,13 +73,31 @@ const shuffle = (arr) => {
   return arr
 }
 
-const cells = 20
-const unitLength = width / cells
+//would like to make this auto adjust
+const autoHeight = Math.floor(window.innerHeight * 0.03)
+const autoWidth = Math.floor(window.innerWidth * 0.03)
+// const autoAdjust = autoHeight
+const cellsHorizontal = autoWidth
+const cellsVertical = autoHeight
 
-const grid = Array(cells)
+const unitLengthX = width / cellsHorizontal
+const unitLengthY = height / cellsVertical
+
+// const unitLengthX = width / autoAdjust
+// const unitLengthY = height / autoAdjust
+
+// const cells = 6
+// const unitLength = width / cells
+
+const grid = Array(cellsVertical)
   .fill(null)
-  .map(() => Array(cells).fill(false))
+  .map(() => Array(cellsHorizontal).fill(false))
 console.log(grid)
+
+// const grid = Array(autoAdjust)
+//   .fill(null)
+//   .map(() => Array(autoAdjust).fill(false))
+// console.log(grid)
 
 //TEST to see if we are modifying 1 or all arrays
 // grid[0].push(true)
@@ -86,18 +105,18 @@ console.log(grid)
 
 //VERTICAL ARRAY
 
-const verticals = Array(cells)
+const verticals = Array(cellsVertical)
   .fill(null)
-  .map(() => Array(cells - 1).fill(false))
+  .map(() => Array(cellsHorizontal - 1).fill(false))
 console.log(verticals)
 
-const horizontals = Array(cells - 1)
+const horizontals = Array(cellsVertical - 1)
   .fill(null)
-  .map(() => Array(cells).fill(false))
+  .map(() => Array(cellsHorizontal).fill(false))
 console.log(horizontals)
 
-const startRow = Math.floor(Math.random() * cells)
-const startColumn = Math.floor(Math.random() * cells)
+const startRow = Math.floor(Math.random() * cellsVertical)
+const startColumn = Math.floor(Math.random() * cellsHorizontal)
 
 console.log(startRow, startColumn)
 
@@ -122,9 +141,9 @@ const stepThroughCell = (row, column) => {
     // See if that neighbor is out of bounds
     if (
       nextRow < 0 ||
-      nextRow >= cells ||
+      nextRow >= cellsVertical ||
       nextColumn < 0 ||
-      nextColumn >= cells
+      nextColumn >= cellsHorizontal
     ) {
       continue
     }
@@ -155,12 +174,16 @@ horizontals.forEach((row, rowIndex) => {
       return
     }
     const wall = Bodies.rectangle(
-      columnIndex * unitLength + unitLength / 2,
-      rowIndex * unitLength + unitLength,
-      unitLength,
+      columnIndex * unitLengthX + unitLengthX / 2,
+      rowIndex * unitLengthY + unitLengthY,
+      unitLengthX,
       5,
       {
+        label: "wall",
         isStatic: true,
+        render: {
+          fillStyle: "red",
+        },
       }
     )
     World.add(world, wall)
@@ -173,12 +196,16 @@ verticals.forEach((row, rowIndex) => {
       return
     }
     const wall = Bodies.rectangle(
-      columnIndex * unitLength + unitLength,
-      rowIndex * unitLength + unitLength / 2,
+      columnIndex * unitLengthX + unitLengthX,
+      rowIndex * unitLengthY + unitLengthY / 2,
       5,
-      unitLength,
+      unitLengthY,
       {
+        label: "wall",
         isStatic: true,
+        render: {
+          fillStyle: "red",
+        },
       }
     )
     World.add(world, wall)
@@ -186,23 +213,33 @@ verticals.forEach((row, rowIndex) => {
 })
 
 //GOAL
+minSquare = Math.min(unitLengthX, unitLengthY) / 2
 const goal = Bodies.rectangle(
-  width - unitLength / 2,
-  height - unitLength / 2,
-  unitLength * 0.7,
-  unitLength * 0.7,
+  width - minSquare,
+  height - minSquare,
+  minSquare * 1.5,
+  minSquare * 1.5,
   {
     label: "goal",
     isStatic: true,
+    render: {
+      fillStyle: "yellow",
+    },
   }
 )
 World.add(world, goal)
 
 //BALL
-const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4, {
+ballRadius = Math.min(unitLengthX, unitLengthY) / 3
+const ball = Bodies.circle(unitLengthX / 2, unitLengthY / 2, ballRadius, {
   label: "ball",
+  render: {
+    fillStyle: "blue",
+  },
 })
 World.add(world, ball)
+
+//Movement
 
 document.addEventListener("keydown", (even) => {
   const {x, y} = ball.velocity
@@ -210,33 +247,40 @@ document.addEventListener("keydown", (even) => {
 
   console.log(event)
   if (event.keyCode === 87) {
-    Body.setVelocity(ball, {x: x, y: y - 5})
+    Body.setVelocity(ball, {x: x, y: y - 2})
     console.log("move up")
   }
   if (event.keyCode === 68) {
-    Body.setVelocity(ball, {x: x + 5, y: y})
+    Body.setVelocity(ball, {x: x + 2, y: y})
     console.log("move right")
   }
   if (event.keyCode === 83) {
-    Body.setVelocity(ball, {x: x, y: y + 5})
+    Body.setVelocity(ball, {x: x, y: y + 2})
     console.log("move down")
   }
   if (event.keyCode === 65) {
-    Body.setVelocity(ball, {x: x - 5, y: y})
+    Body.setVelocity(ball, {x: x - 2, y: y})
     console.log("move left")
   }
 })
 
 //Win Condition
 
-Events.om(engine, "collisionStart", (event) => {
+Events.on(engine, "collisionStart", (event) => {
   event.pairs.forEach((collision) => {
     const labels = ["ball", "goal"]
     if (
       labels.includes(collision.bodyA.label) &&
       labels.includes(collision.bodyB.label)
     ) {
+      document.querySelector(".winner").classList.remove("hidden")
       console.log("User Won!")
+      world.gravity.y = 1
+      world.bodies.forEach((body) => {
+        if (body.label === "wall") {
+          Body.setStatic(body, false)
+        }
+      })
     }
   })
 })
